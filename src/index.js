aaa8d3b855a131140dc08171e6482df180b6056c
nitin <mneonizer@gmail.com>
2022-02-20 19:48:55 +0530

    updated text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/home';
